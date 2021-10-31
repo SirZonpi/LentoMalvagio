e376f419490146afc5e5dcb3815fb173b737580d
RobertoN <roberto.nautilus@gmail.com>
2021-11-01 00:43:49 +0100

    FINITO

---
+++ Assets/Scripts/Roberto/Attivatore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attivatore : MonoBehaviour
+{
+    //Roberto
+    public bool openDoor;
+
+
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+            
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E)) //se sono nel cerchio CanTakeTheKey e premo E
+        {
+            openDoor = true; //setto KeyTaken a true
+        }
+    }
+}
---
+++ Assets/Scripts/Roberto/Porta.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porta : MonoBehaviour
+{
+    //Roberto Niciforo
+    public Attivatore attDaAttivare;
+    public GameObject porta;
+    public GameObject attivatore;
+    public Material attivatoreAttivo;
+    public Animator door;
+    private void Update()
+    {
+        
+         if (attDaAttivare.openDoor == true) //e sono dentro isInRange e ho preso la chiave assegnata.
+         {
+            attivatore.GetComponent<MeshRenderer>().material = attivatoreAttivo;
+            door.SetBool("apriPorta", true);
+         }
+        else
+            door.SetBool("apriPorta", false);
+
+
+
+    }
+
+}
