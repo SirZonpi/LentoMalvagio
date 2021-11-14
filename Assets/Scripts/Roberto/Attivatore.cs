27d9c7590c962dcdd66215a4b6bd61eec8188c73
emanuelefazio <emanuelefaziovfx@gmail.com>
2021-11-14 14:06:45 +0100

    interruttore e fix

--- Assets/Scripts/Roberto/Attivatore.cs
+++ Assets/Scripts/Roberto/Attivatore.cs
@@ -8,20 +8,9 @@ public class Attivatore : MonoBehaviour
     public bool openDoor;
 
 
-
-    private void Start()
+    private void OnTriggerEnter(Collider other)
     {
-
-    }
-
-    void Update()
-    {
-            
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (Input.GetKeyDown(KeyCode.E)) //se sono nel cerchio CanTakeTheKey e premo E
+        if (other.CompareTag("SpadaPlayer") || other.CompareTag("SpellPlayer")) //se sono nel cerchio CanTakeTheKey e premo E
         {
             openDoor = true; //setto KeyTaken a true
         }
